e2945ffa072ee5e43a45e68175debbc3460e219e
gugus400 <hans.chnuspi@gmx.ch>
2018-10-05 11:38:02 +0200

    Find out mouse position

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("jawollo")
 console.log("lorem ipsum dolor sit amet")
 window.onclick = (e) => {
-	console.log(e)
+	let x = e.clientX
+	let y = e.clientY
+	console.log("x: " + x  + " y: " + y)
 }
