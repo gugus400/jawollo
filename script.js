d1622a063d8e915b33a7113ea0b73646228ef534
gugus400 <hans.chnuspi@gmx.ch>
2018-10-05 11:42:01 +0200

    Add position display

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const position = document.getElementById("position")
+
 console.log("jawollo")
 console.log("lorem ipsum dolor sit amet")
 window.onclick = (e) => {
 	let x = e.clientX
 	let y = e.clientY
 	console.log("x: " + x  + " y: " + y)
+
+	position.style.left = x
+	position.style.top = y
 }
